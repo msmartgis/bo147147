7b317a293c367269f266df87ce3d32eabaa866a5
MED <mohamedelhanine94@gmail.com>
2020-02-22 12:27:50 +0100

    commit

---
+++ resources/js/Firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD_vc9pYsaE5fPrAf_XCsk_RAALvTVaNSI",
+    authDomain: "bureau-ordre-firebase.firebaseapp.com",
+    databaseURL: "https://bureau-ordre-firebase.firebaseio.com",
+    projectId: "bureau-ordre-firebase",
+    storageBucket: "bureau-ordre-firebase.appspot.com",
+    messagingSenderId: "927104865964",
+    appId: "1:927104865964:web:a68c2b369fe75a26a87109"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="_token" content="{{csrf_token()}}" />
+    <meta name="user-role" content="{{ Auth::user()->role->first()->role_name }}">
     <meta name="local" content="{{Config::get('app.locale')}}" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -167,7 +168,6 @@
     display: inline-block;
     position: relative;
     border-radius: 0.2em;
-    font-size: 1.3em;
 }
 
 .notification::before, 
@@ -184,17 +184,17 @@
 }
 
 .notification::after {
+    color: white;
     font-family: Arial;
-    font-size: 0.7em;
+    font-size: 15px;
     font-weight: 700;
     position: absolute;
-    top: -15px;
+    top: 2px;
     right: -15px;
     padding: 5px 8px;
     line-height: 100%;
-    border: 2px #fff solid;
     border-radius: 60px;
-    background: #3498db;
+    background: #d11124;
     opacity: 0;
     content: attr(data-count);
     opacity: 0;
@@ -251,8 +251,6 @@
                     </a>
 
                     <ul class="navbar-nav mr-auto mt-md-0">
-
-
                     </ul>
 
                     <div class="navbar-custom-menu">
@@ -262,41 +260,39 @@
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                     <div class="notification"></div>
                                 </a>
-                                <ul class="dropdown-menu scale-up">
+                                <ul class="dropdown-menu scale-up"  >
                                 <li class="header" style="text-align: center;font-weight: 600;">Vous avez {{Auth()->user()->unreadNotifications->count()}} notifications</li>
-                                <li>
-                                    <!-- inner menu: contains the actual data -->
-                                    <ul class="menu inner-content-div">
-                                        @foreach(Auth()->user()->unreadNotifications as $notification)
-                                            <li>                                                                                              
-                                                <a
-                                                    @if ($notification->data['type_courrier'] == "entrant")
-                                                        href="{{ route('courriers-entrants.edit', ['courriers_entrant' => $notification->data['id'] ]) }}"
+                                <li id="notification_list">
+                                    <!-- inner menu: contains the actual data -->                                   
+                                        <ul class="menu inner-content-div">                                           
+                                            {{-- <li><a><i class="fa fa-arrow-right text-yellow"></i></a></li> --}}
+                                            {{-- @foreach(Auth()->user()->unreadNotifications as $notification)
+                                                <li>                                                                                              
+                                                    <a
+                                                        @if ($notification->data['type_courrier'] == "entrant")
+                                                            href="{{ route('courriers-entrants.edit', ['courriers_entrant' => $notification->data['id'] ]) }}"
+                                                        @endif
+
+                                                        @if ($notification->data['type_courrier'] == "sortant")
+                                                            href="{{ route('courriers-sortants.edit', ['courriers_sortant' => $notification->data['id'] ]) }}"
+                                                        @endif                                            
+                                                        target="_blank">
+                                                    <i 
+                                                    @if ($notification->data['type_courrier'] == "entrant" )
+                                                    class="fa fa-arrow-left text-green" 
+                                                    @else
+                                                    class="fa fa-arrow-right text-yellow" 
                                                     @endif
-
-                                                    @if ($notification->data['type_courrier'] == "sortant")
-                                                        href="{{ route('courriers-sortants.edit', ['courriers_sortant' => $notification->data['id'] ]) }}"
-                                                    @endif                                            
-                                                    target="_blank">
-                                                <i 
-                                                @if ($notification->data['type_courrier'] == "entrant" )
-                                                class="fa fa-arrow-left text-green" 
-                                                @else
-                                                class="fa fa-arrow-right text-yellow" 
-                                                @endif
-                                                ></i>                                                     
-                                                     {{ Str::limit($notification->data['objet'] , 30)   }}
-
-                                                    
-                                                     <div class="row">
-                                                        <span style="color: darkgrey;">{{$notification->created_at}}</span>  
-                                                    </div>
-                                                    
-                                                </a>
-                                            </li>                                        
-                                        @endforeach                              
-                                
-                                    </ul>
+                                                    ></i>                                                     
+                                                        {{ Str::limit($notification->data['objet'] , 30)   }}                                                    
+                                                        <div class="row">
+                                                            <span style="color: darkgrey;">{{$notification->created_at}}</span>  
+                                                        </div>
+                                                        
+                                                    </a>
+                                                </li>                                        
+                                            @endforeach --}}
+                                        </ul>  
                                 </li>
                                 @if (Auth()->user()->unreadNotifications->count() > 0)
                             <li class="footer"><a href="{{ route('markNotificationsAsRead')}}"><i class="fa fa-check text-orange"></i> Marquer tous comme lus</a></li> 
@@ -327,7 +323,6 @@
                                     <!-- Menu Body -->
                                     <li class="user-body">
                                         <div class="row no-gutters">
-
                                             <div role="separator" class="divider col-12"></div>
                                             <div class="col-12 text-left">
                                                 <a class="dropdown-item" href="/profile">
@@ -369,6 +364,32 @@
         <div class="content-wrapper {{__('costum_css.body')}}">
             <!-- Main content -->
             <section class="content m-content">
+
+                <!-- The core Firebase JS SDK is always required and must be listed first -->
+                <script src="https://www.gstatic.com/firebasejs/7.8.2/firebase-app.js"></script>
+                <script src="https://www.gstatic.com/firebasejs/7.8.2/firebase-firestore.js"></script>
+                <script src="https://www.gstatic.com/firebasejs/7.8.2/firebase-auth.js"></script>
+                <script src="https://www.gstatic.com/firebasejs/7.8.2/firebase-database.js"></script>
+                <!-- TODO: Add SDKs for Firebase products that you want to use
+                    https://firebase.google.com/docs/web/setup#available-libraries -->
+
+                <script>
+                // Your web app's Firebase configuration
+                var firebaseConfig = {
+                    apiKey: "AIzaSyD_vc9pYsaE5fPrAf_XCsk_RAALvTVaNSI",
+                    authDomain: "bureau-ordre-firebase.firebaseapp.com",
+                    databaseURL: "https://bureau-ordre-firebase.firebaseio.com",
+                    projectId: "bureau-ordre-firebase",
+                    storageBucket: "bureau-ordre-firebase.appspot.com",
+                    messagingSenderId: "927104865964",
+                    appId: "1:927104865964:web:a68c2b369fe75a26a87109"
+                };
+                // Initialize Firebase
+                firebase.initializeApp(firebaseConfig);
+                </script>
+
+
+            
                 @include('inc.messages')
                 @yield('content')
 
@@ -379,9 +400,6 @@
 
         <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
         <div class="control-sidebar-bg"></div>
-
-
-
     </div>
     <!-- ./wrapper -->
     <script>
@@ -391,30 +409,83 @@
 </body>
 
 <script>
+ 
+    //notification firebase
+    var submitBtn = document.getElementById('firebase_btn'); 
 
-    var el = document.querySelector('.notification');
+    var firebaseNotificationRef = firebase.database().ref("/Notifications");
+    var logged_user = $('meta[name=user-role]').attr("content");
+    
+  
+   
 
-    $.ajax({
-        url: "/services/get_service",
-        type: "GET",
-        dataType: 'JSON',
-        data: {
-            service_id: service_id
-        },
-        success: function (res) {       
+    //console.log(firebaseElementsRef);
+    firebaseNotificationRef.on('value', function(datasnapshot){       
+        var notification_number = 0;
+        var list_to_append = "";
+        
+        datasnapshot.forEach(function(childSnapshot) {            
+            var childDataNotification = childSnapshot.val();
+            var arrow_icon;
+            if(logged_user === childDataNotification.user_to_notify)
+            {                
+                notification_number++;           
 
-        }
-    });
+                switch(childDataNotification.type) {
+                case "courrier_entrant":
+                    arrow_icon = "fa fa-arrow-left text-green";
+                    break;
+                case "courrier_sortant":
+                    arrow_icon = "fa fa-arrow-right text-yellow";
+                    break;
+                default:
+                    arrow_icon = "fa fa-info text-yellow";
+                }
 
+                list_to_append += '<li>'+
+                '<a>'+
+                '<i  class="'+arrow_icon+'" ></i>'
+                +childDataNotification.objet+
+                '</a>'+
+                '</li>';
+            }       
+            
+        });
+
+        $('#notification_list').html(list_to_append)
+
+        
+        var el = document.querySelector('.notification');
         var count = Number(el.getAttribute('data-count')) || 0;
-        el.setAttribute('data-count', count + 1);
+        el.setAttribute('data-count', notification_number);
+       
         el.classList.remove('notify');
         el.offsetWidth = el.offsetWidth;
         el.classList.add('notify');
+
         if(count === 0){
             el.classList.add('show-count');
         }
- 
+
+    });
+
+  
+    
+    // function submitToFirebase()
+    // {
+    //   var firebaseRef = firebase.database().ref("/Notifications");
+    //   firebaseRef.push({
+    //       type : "CourrierAdded",
+    //       user_id : 7,
+    //       data : {
+    //         type_courrier : "Sortant",
+    //         id : "c831e2e0-4daa-11ea-b5fd-b909c4f7f2f6",
+    //         objet : "objet sortant",
+    //       },
+    //   });
+    // }
+
+    // end notification firebase
 </script>
 
 </html>
